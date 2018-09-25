b1eb2588c5520af18e1f621c76e465b0826da827
Adam Carter <mail@adam-carter.com>
2018-09-24 19:34:45 -0700

    added tomorrow's starter lecture.

---
+++ lectures/2018-09-25 C++ - Conditionals/lecture/lecture/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //print to console, read from keyboard
+#include <string>   //gives us string variable type
+#include <fstream>  //allows us to write to a file
+using namespace std;
+int main()
+{
+    /*
+	Agenda: 
+		PA #3 Tips
+			1. Writing logic to match input
+			2. Making sure output window is correct
+		Exploring logic through C++ programming challengs:
+			*. Check for division by zero
+			*. Determining if a number is a 3 digit number
+			*. Convert a 12hr time (e.g. 3:00 AM) into 24 hour time
+			*. Convert a 24hr time (e.g. 18:35) into 12 hour time
+			*. Determining if a character is also a number
+			*. Using an array, input 3 numbers from the user, output in sorted order
+	*/
+}
