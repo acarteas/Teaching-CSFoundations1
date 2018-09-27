c159bba1b6e4bccf88f0c87486e0cf4e04449b32
Adam Carter <mail@adam-carter.com>
2018-09-27 10:30:11 -0700

    Added starter lecture code

---
+++ lectures/2018-09-27 C++ - Conditionals 2/lecture/lecture/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //print to console, read from keyboard
+#include <string>   //gives us string variable type
+#include <fstream>  //allows us to write to a file
+#include <iomanip>
+using namespace std;
+int main()
+{
+    /*
+	Agenda: 
+        1. No office hours today
+        2. Cover PA #3
+        3. Determining if a character is also a number
+	    4. Using an array, input 3 numbers from the user, output in sorted order
+        5. Introduction to loops
+        6. Finding the average of a series of numbers
+        7. Finding the average of a series of numbers in a file
+        8. Finding the largest and smallest number in a file
+	*/
+
+}
