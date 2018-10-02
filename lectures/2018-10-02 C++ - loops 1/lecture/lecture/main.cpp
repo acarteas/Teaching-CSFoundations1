fe7bfd32064c8d776a388bbf8eb1b3a78d3c42dd
Adam Carter <mail@adam-carter.com>
2018-10-01 19:20:39 -0700

    Added tomorrow's starter code to the repository.

---
+++ lectures/2018-10-02 C++ - loops 1/lecture/lecture/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream> //print to console, read from keyboard
+#include <string>   //gives us string variable type
+#include <fstream>  //allows us to write to a file
+#include <iomanip>
+#include <ctime> //for RNG
+#include <vector> //fancy array
+using namespace std;
+int main()
+{
+    /*
+	Agenda: 
+        *. Introduce PA #4
+		*. Practice algorithm #1:
+				Until the user enters a negative number:
+					prompt the user for a number
+					If this number is postive, add to a "running total"
+        *. Finding the largest and smallest number in a file
+		*. How many 'a' characters are in a string?
+		*. Convert all characters in a string to lower case
+		*. Add 1 to every value in a sequence
+		*. Sorting (selection sort & bubble sort)
+		*. 
+	*/
+
+}
