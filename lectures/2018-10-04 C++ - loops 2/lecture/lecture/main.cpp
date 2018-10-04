217b7efe5dbaf63cc167a46b9cd4a4b549170440
Adam Carter <mail@adam-carter.com>
2018-10-03 20:18:01 -0700

    added tomorrow's starter lecture code.

---
+++ lectures/2018-10-04 C++ - loops 2/lecture/lecture/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream> //print to console, read from keyboard
+#include <string>   //gives us string variable type
+#include <fstream>  //allows us to write to a file
+#include <iomanip>
+#include <ctime> //for RNG
+#include <vector> //fancy array
+using namespace std;
+int main()
+{
+    /*
+	Agenda: 
+        *. Bowling Night Friday @ 6!
+		*. Mid-semester check-in
+		*. Fibonacci Sequence
+		*. Sorting 
+		*. Convert every first letter in a phrase to upper case
+		*.		same as above, but this time ignore common words
+		*. Drawing a tree (book 6.5.1)
+		*. Code cracker (all permutations of a N-digit string)
+		*. 
+	*/
+
+}
