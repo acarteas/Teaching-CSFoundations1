b40ba92b0c66e9d7389ff2e74df50693a4d5b5ce
Adam Carter <mail@adam-carter.com>
2018-10-30 09:50:29 -0700

    Added today's starter lecture code.

---
+++ lectures/2018-10-30 C++ - Functions 1/lecture/lecture/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream> //print to console, read from keyboard
+#include <string>   //gives us string variable type
+#include <fstream>  //allows us to write to a file
+#include <iomanip>
+#include <ctime> //for RNG
+#include <vector> //fancy array
+using namespace std;
+int main()
+{
+    /*
+	Agenda: 
+		*. Calendar just says CH8 in book.  Also look at CH9.
+        *. Discuss basic function syntax in C++
+			Putting function prototype above main
+			Putting function definition after main
+		*. Write a function that finds the maximum of two numbers
+		*. Write a RNG dice roller
+		*. Write a square root function
+		*. Write a function that prompts a user for a number
+			Include error checking
+		*. Review: pointers and references
+		*. Swapping values using pass by pointer
+		*. Passing objects by reference
+		*. Filling a vector with random numbers
+		*. Finding the smallest item in an array
+	*/
+
+}
