61c2f8ee921cac99d9e930877f1f4b2d089c57d5
Adam Carter <mail@adam-carter.com>
2018-11-27 14:18:28 -0800

    added today's final lecture code.

---
+++ lectures/2018-11-27 JS - Student Records/Student.js
@@ -0,0 +1,11 @@
+class Student
+{
+   constructor()
+   {
+      this.name = "";
+      this.gpa = 0.0;
+      this.class_standing = "Freshman";
+      this.age = 18;
+      this.major = "Computer Science";
+   }
+}
